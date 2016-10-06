b46110c54a352f18b4dd8d333191269dfca7bd36
Nikhil D'Souza <nikhiljay7@gmail.com>
2016-10-06 00:36:31 -0700

    Quick fix to Week 1 Curriculum.

--- Week-1/Week-1/GameScene.swift
+++ Week-1/Week-1/GameScene.swift
@@ -11,11 +11,7 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var lastUpdateTime : TimeInterval = 0
-    
     override func sceneDidLoad() {
-        self.lastUpdateTime = 0
-        
         let square = SKShapeNode(rectOf: CGSize(width: 200, height: 200)) //create square and initialize with size
         square.position = CGPoint(x: size.width / 2, y:  size.height / 2) //set position of square
         square.fillColor = SKColor.blue //color of square
